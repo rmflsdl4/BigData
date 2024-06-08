5a7f5bb122529579908bf657594f7638aa60538b
rmflsdl4 <rmflsdl4@gmail.com>
2024-06-09 00:14:17 +0900

    업데이트
    1. 숙박 업체를 행정안전부애서 제공하는 데이터로 불러옴
    2. 랜덤으로 숙박 업체를 고른 후 해당 지점을 지도의 중심으로 수정

--- DataLoad.py
+++ DataLoad.py
@@ -18,10 +18,10 @@ def coordinateToFormat(x, y):
 
 def createMap(stores):
     # 위도, 경도
-    lat, lon = stores[2]['lat'], stores[2]['lon']
+    lat, lon = stores[0]['lat'], stores[0]['lon']
 
-    map = folium.Map([lat, lon], zoom_start = 16)
-    maxIdx = 4
+    map = folium.Map([lat, lon], zoom_start = 18)
+    maxIdx = len(stores)
 
     # 마커 아이콘
     for i in range(maxIdx):
@@ -35,6 +35,9 @@ def createMap(stores):
         if stores[i]['업태구분명'] == '기타' or stores[i]['업태구분명'] == '커피숍':
             iconColor = 'red'
             iconType = 'time'
+        elif i == 0:
+            iconColor = 'lightblue'
+            iconType = 'home'
         else:
             iconColor = 'orange'
             iconType = 'cutlery'
@@ -62,9 +65,13 @@ def openMap():
     # 웹 브라우저를 사용하여 HTML 파일 열기
     webbrowser.open(mapPath)
 
-def createRoutine(stores, bestStoreName):
+def createRoutine(stores, accommodation, bestStoreName):
     routine = []
     
+
+    # 랜덤 숙소 선정
+    routine.append(accommodation.iloc[random.randint(0, len(accommodation)-1)])
+
     filteredBreakfast = stores[(stores['업태구분명'] != '기타') & (stores['업태구분명'] != '호프/통닭') & (stores['업태구분명'] != '커피숍')]
     filteredLunch = stores[(stores['업태구분명'] != '기타') & (stores['업태구분명'] != '호프/통닭') & (stores['업태구분명'] != '커피숍')]
     filteredEtc = stores[(stores['업태구분명'] == '기타') | (stores['업태구분명'] == '커피숍')]
@@ -105,10 +112,11 @@ def createRoutine(stores, bestStoreName):
         routine.append(dinnerBest.iloc[random.randint(0, len(dinnerBest)-1)])  
 
 
-    print(f"아침: {routine[0]['사업장명']} , {routine[0]['업태구분명']}, {routine[0]['lat']}, {routine[0]['lon']}")
-    print(f"점심: {routine[1]['사업장명']} , {routine[1]['업태구분명']}")
-    print(f"식후: {routine[2]['사업장명']} , {routine[2]['업태구분명']}")
-    print(f"저녁: {routine[3]['사업장명']} , {routine[3]['업태구분명']}")
+    print(f"숙소: {routine[1]['사업장명']}")
+    print(f"아침: {routine[1]['사업장명']} , {routine[1]['업태구분명']}")
+    print(f"점심: {routine[2]['사업장명']} , {routine[2]['업태구분명']}")
+    print(f"식후: {routine[3]['사업장명']} , {routine[3]['업태구분명']}")
+    print(f"저녁: {routine[4]['사업장명']} , {routine[4]['업태구분명']}")
     
     return routine
 
@@ -117,27 +125,29 @@ def createRoutine(stores, bestStoreName):
 bakeryFilePath='./CSV/bakery.csv'
 recessFilePath='./CSV/recess.csv'
 normalFilePath='./CSV/normal.csv'
+accommodationFilePath='./CSV/accommodation.csv'
 bestStoreNamePath='./CSV/bestStoreName.csv'
 
 # 필요한 열들을 리스트로 묶음
 initColumns = ['소재지전체주소', '도로명전체주소', '도로명우편번호', '사업장명', '업태구분명', '좌표정보(x)', '좌표정보(y)']
+accommodationColumns = ['소재지전체주소', '도로명전체주소', '사업장명', '좌표정보(x)', '좌표정보(y)']
 columns = ['소재지전체주소', '도로명전체주소', '도로명우편번호', '사업장명', 'lon', 'lat']
 
 # 음식점 데이터 로드
 bakery = pd.read_csv(bakeryFilePath, encoding="CP949", usecols=initColumns, dtype={'좌표정보(x)': float, '좌표정보(y)': float})
 recess = pd.read_csv(recessFilePath, encoding="CP949", usecols=initColumns, dtype={'좌표정보(x)': float, '좌표정보(y)': float})
 normal = pd.read_csv(normalFilePath, encoding="CP949", usecols=initColumns, dtype={'좌표정보(x)': float, '좌표정보(y)': float})
+accommodation = pd.read_csv(accommodationFilePath, encoding="CP949", usecols=initColumns, dtype={'좌표정보(x)': float, '좌표정보(y)': float})
 bestStoreName = pd.read_csv(bestStoreNamePath, usecols=['상호명'])
 
 
 # 위 3개의 데이터를 합침
 totalData = pd.concat([bakery, recess, normal])
 print(totalData)
-# 좌표계를 정수값으로 변환
-totalData['좌표정보(x)'] = pd.to_numeric(totalData['좌표정보(x)'], errors="coerce")
-totalData['좌표정보(y)'] = pd.to_numeric(totalData['좌표정보(y)'], errors="coerce")
 
+# x, y 좌표를 위도, 경도로 수정
 totalData['lon'], totalData['lat'] = coordinateToFormat(totalData['좌표정보(x)'].values, totalData['좌표정보(y)'].values)
+accommodation['lon'], accommodation['lat'] = coordinateToFormat(accommodation['좌표정보(x)'].values, accommodation['좌표정보(y)'].values)
 
 
 
@@ -149,13 +159,17 @@ searchData = totalData.loc[
                         totalData['도로명전체주소'].str.contains(inuptAddress) |
                         totalData['사업장명'].str.contains(inuptAddress)
                     ] 
+filterAccommodation = accommodation.loc[
+                        accommodation['소재지전체주소'].str.contains(inuptAddress) |
+                        accommodation['도로명전체주소'].str.contains(inuptAddress) 
+                    ] 
 stores = searchData[['도로명전체주소','사업장명', 'lon', 'lat', '업태구분명', '소재지전체주소']]
 
 # 요청 보내기
 print("검색된 가게 목록: ", len(stores))
 
 # 아침 - 점심 - 식후 - 저녁 - 랜덤하게 영화관 등의 장소
-routine = createRoutine(stores, bestStoreName)
+routine = createRoutine(stores, filterAccommodation, bestStoreName)
 # 지도 생성
 createMap(routine)
 
